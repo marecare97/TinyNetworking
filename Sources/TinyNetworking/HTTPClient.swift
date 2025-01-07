c45b7528dcfd22b3818d9f5895236b64dcffdede
Marko <markosentivanac1@gmail.com>
2025-01-07 20:14:57 +0100

    adds more nedded files

---
+++ Sources/TinyNetworking/HTTPClient.swift
@@ -0,0 +1,73 @@
+//
+//  TinyNetworking.swift
+//  TinyNetworking
+//
+//  Created by Joan Disho on 02.03.18.
+//  Copyright © 2018 Joan Disho. All rights reserved.
+//
+
+import Foundation
+import os
+
+public class HTTPClient<R: HTTPResource>: HTTPClientType {
+    
+    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!,
+                                category: String(describing: HTTPClient.self))
+    
+    public init() {}
+    
+    public func requestAsync(
+        resource: R,
+        session: HTTPClientSession = URLSession.shared
+    ) async -> Result<HTTPResponse, HTTPError> {
+        let result = await executeRequest(resource: resource, session: session)
+        if case .failure(let error) = result, resource.shouldRetry(from: error) {
+            await resource.retryCallback(httpClient: self)
+            return await executeRequest(resource: resource, session: session)
+        }
+        return result
+    }
+    
+    private func executeRequest(resource: any HTTPResource, session: HTTPClientSession) async -> Result<HTTPResponse, HTTPError> {
+        var request = URLRequest(resource: resource)
+        if resource.isAuthorized {
+            resource.authorize(request: &request)
+        }
+        logger.logRequest(request)
+        let (response, error) = await session.loadDataAsync(with: request)
+        if error != nil {
+            logger.logError(response, error!)
+            return .failure(.networkLayer(error!, response))
+        } else {
+            guard
+                let httpURLResponse = response.httpURLResponse,
+                200..<300 ~= httpURLResponse.statusCode else {
+                logger.logError(response, HTTPError.statusCode(response))
+                return .failure(.statusCode(response))
+            }
+            logger.logResponse(response)
+            return .success(response)
+        }
+    }
+}
+
+extension Result {
+    var isSuccess: Bool {
+        switch self {
+        case .success:
+            return true
+        case .failure:
+            return false
+        }
+    }
+    
+    var successValue: Success? {
+        switch self {
+        case .success(let value):
+            return value
+        case .failure:
+            return nil
+        }
+    }
+}
+
